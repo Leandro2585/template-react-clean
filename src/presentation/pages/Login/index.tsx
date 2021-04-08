44ece4052319caa4bd51611495796a7e70a5349b
Leandro Real <leo.real2585@gmail.com>
2021-04-08 14:24:36 -0300

    feat: ensure Login present error if password is invalid

--- src/presentation/pages/Login/index.tsx
+++ src/presentation/pages/Login/index.tsx
@@ -14,20 +14,18 @@ const Login: React.FC<Props> = ({ validation }: Props) => {
     email: '',
     password: '',
     emailError: '',
-    passwordError: 'Campo obrigatório',
+    passwordError: '',
     mainError: ''
   })
 
   useEffect(() => {
     setState({
       ...state,
-      emailError: validation.validate('email', state.email)
-    })
-  }, [state.email])
+      emailError: validation.validate('email', state.email),
+      passwordError: validation.validate('password', state.password)
 
-  useEffect(() => {
-    validation.validate('password', state.password)
-  }, [state.password])
+    })
+  }, [state.email, state.password])
 
   return (
     <div className={styles.login}>
