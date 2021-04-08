2ef3d84f45dddedeb5630a41f1e8cd00be9f2f5f
Leandro Real <leo.real2585@gmail.com>
2021-04-08 09:25:29 -0300

    refactor: move sut creation to a factory method

--- src/infra/http/AxiosHttpClient.spec.ts
+++ src/infra/http/AxiosHttpClient.spec.ts
@@ -20,13 +20,13 @@ const mockPostRequest = (): HttpPostParams<any> => ({
 })
 
 describe('AxiosHttpClient', () => {
-  test('Should call axios with correct values', async () => {
+  test('should call axios with correct values', async () => {
     const request = mockPostRequest()
     const sut = makeSut()
     await sut.post(request)
     expect(mockedAxios.post).toHaveBeenCalledWith(request.url, request.body)
   })
-  test('Should return the correct statusCode and body', async () => {
+  test('should return the correct statusCode and body', async () => {
     const sut = makeSut()
     const httpResponse = await sut.post(mockPostRequest())
     expect(httpResponse).toEqual({
