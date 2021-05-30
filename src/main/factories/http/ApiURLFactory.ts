91d938f030178492df59d69d1ed90030a5263eac
Leandro Real <leo.real2585@gmail.com>
2021-05-30 16:46:41 -0300

    fix: change webpack env var to use JSON.stringify

--- src/main/factories/http/ApiURLFactory.ts
+++ src/main/factories/http/ApiURLFactory.ts
@@ -2,5 +2,5 @@ export const makeApiUrl = (path: string): string => {
   if (path[0] !== '/') {
     path.padStart(path.length + 1, '/')
   }
-  return `process.env.API_URL${path}`
+  return `${process.env.API_URL}${path}`
 }
--- webpack.config.js
+++ webpack.config.js
@@ -59,7 +59,7 @@ module.exports = {
   plugins: [
     new CleanWebpackPlugin(),
     new DefinePlugin({
-      'process.env.API_URL': 'http://localhost:3333/api'
+      'process.env.API_URL': JSON.stringify('http://localhost:3333/api')
     })
   ]
 }
