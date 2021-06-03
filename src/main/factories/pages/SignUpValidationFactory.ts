e7dadd02de221537b09990601ca860f6f4bafe93
Leandro Real <leo.real2585@gmail.com>
2021-06-03 14:14:41 -0300

    feat: add SignUpValidationFactory

---
+++ src/main/factories/pages/SignUpValidationFactory.ts
@@ -0,0 +1,10 @@
+import { ValidationBuilder, ValidationComposite } from '@validation/validators'
+
+export const makeSignUpValidation = (): ValidationComposite => {
+  return ValidationComposite.build([
+    ...ValidationBuilder.field('name').required().build(),
+    ...ValidationBuilder.field('email').required().email().build(),
+    ...ValidationBuilder.field('password').required().min(6).build(),
+    ...ValidationBuilder.field('confirmPassword').required().min(5).build()
+  ])
+}
