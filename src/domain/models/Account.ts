ee3217edc937d75315fc3f6e8d56a5ba19926e50
Leandro Real <leo.real2585@gmail.com>
2021-06-05 11:35:37 -0300

    refactor: move type models of usecases to a separate files

---
+++ src/domain/models/Account.ts
@@ -0,0 +1,6 @@
+export type AccountModel = {
+  name: string;
+  email: string;
+  password: string;
+  confirmPassword: string;
+}
--- src/domain/usecases/AddAccount.ts
+++ src/domain/usecases/AddAccount.ts
@@ -1,15 +1,11 @@
+import { AccountModel } from '@domain/models'
+
 export interface AddAccount {
   create(params: AddAccount.Params): Promise<AddAccount.Result>;
 }
 
 export namespace AddAccount {
-  export type Params = {
-    name: string;
-    email: string;
-    password: string;
-    confirmPassword: string;
-  }
-
+  export type Params = AccountModel
   export type Result = {
     accessToken: string;
   }
