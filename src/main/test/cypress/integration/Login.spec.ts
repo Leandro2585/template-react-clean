1d2bdd28863dbc7ee16ee8f1c862b0dcb7610f0d
Leandro Real <leo.real2585@gmail.com>
2021-06-05 07:43:03 -0300

    test: prevent Login to call submit multiple times

--- src/main/test/cypress/integration/Login.spec.ts
+++ src/main/test/cypress/integration/Login.spec.ts
@@ -116,4 +116,19 @@ describe('Login', () => {
     cy.url().should('eq', `${baseUrl}/`)
     cy.window().then(window => assert.isOk(window.localStorage.getItem('accessToken')))
   })
+
+  it('should prevent multiple submits', () => {
+    cy.route({
+      method: 'POST',
+      url: /login/,
+      status: 200,
+      response: {
+        accessToken: faker.datatype.uuid()
+      }
+    }).as('request')
+    cy.getByTestId('email').focus().type(faker.internet.email())
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(6))
+    cy.getByTestId('submit').dblclick()
+    cy.get('request.all').should('have.length', 1)
+  })
 })
