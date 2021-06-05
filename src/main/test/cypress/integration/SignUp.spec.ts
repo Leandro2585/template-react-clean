00d34497e29fd43b2945184b8c88f008cc0a5053
Leandro Real <leo.real2585@gmail.com>
2021-06-05 09:41:48 -0300

    test: ensure SignUp load with correct state

---
+++ src/main/test/cypress/integration/SignUp.spec.ts
@@ -0,0 +1,20 @@
+import * as FormHelper from '../support/FormHelper'
+
+describe('SignUp', () => {
+  beforeEach(() => {
+    cy.visit('signup')
+  })
+
+  it('should load with correct initial state', () => {
+    cy.getByTestId('name').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('name', 'Campo obrigatório')
+    cy.getByTestId('email').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('email', 'Campo obrigatório')
+    cy.getByTestId('password').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('password', 'Campo obrigatório')
+    cy.getByTestId('confirmPassword').should('have.attr', 'readOnly')
+    FormHelper.testInputStatus('confirmPassword', 'Campo obrigatório')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+})
