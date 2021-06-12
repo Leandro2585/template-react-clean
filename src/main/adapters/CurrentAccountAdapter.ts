77f16d41d3f9933ca48affd1a32032e29b082932
Leandro2585 <leo.real2585@gmail.com>
2021-06-12 14:59:50 -0300

    feat: ensure LocalStorageAdapter calls localStorage.getItem with correct value

--- src/main/adapters/CurrentAccountAdapter.ts
+++ src/main/adapters/CurrentAccountAdapter.ts
@@ -8,3 +8,7 @@ export const setCurrentAccountAdapter = (account: AccountModel): void => {
   }
   makeLocalStorageAdapter().set('@4Devs:account', account)
 }
+
+export const getCurrentAccountAdapter = (): AccountModel => {
+  return makeLocalStorageAdapter().get('@4Devs:account')
+}
