688fcff3befe2656904680d255a6193863202a61
Leandro2585 <leo.real2585@gmail.com>
2021-06-12 21:44:13 -0300

    test: ensure SurveyList Component calls LoadSurveysList

--- src/shared/pages/SurveyList.spec.tsx
+++ src/shared/pages/SurveyList.spec.tsx
@@ -1,9 +1,26 @@
 import React from 'react'
 import { render, screen } from '@testing-library/react'
 import { SurveyList } from '@shared/pages'
+import { LoadSurveysList } from '@domain/usecases'
 
-const makeSut = (): void => {
-  render(<SurveyList/>)
+class LoadSurveysListSpy implements LoadSurveysList {
+  callsCount = 0
+  async loadAll (): Promise<LoadSurveysList.Model> {
+    this.callsCount++
+    return []
+  }
+}
+
+type SutTypes = {
+  loadSurveyListSpy: LoadSurveysListSpy;
+}
+
+const makeSut = (): SutTypes => {
+  const loadSurveyListSpy = new LoadSurveyListSpy()
+  render(<SurveyList loadSurveyList={loadSurveyListSpy}/>)
+  return {
+    loadSurveyListSpy
+  }
 }
 
 describe('SurveyList Component', () => {
@@ -11,5 +28,10 @@ describe('SurveyList Component', () => {
     makeSut()
     const surveyList = screen.getByTestId('survey-list')
     expect(surveyList.querySelectorAll('li:empty').length).toBe(4)
+  })
+
+  test('should call LoadSurveyList', () => {
+    const { loadSurveyListSpy } = makeSut()
+    expect(loadSurveyListSpy.callsCount).toBe(1)
   })
 })
