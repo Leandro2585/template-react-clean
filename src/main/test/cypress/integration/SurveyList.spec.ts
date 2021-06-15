b388c20b0d1cd450ce3427c51c8b6e39adbeb021
Leandro2585 <leo.real2585@gmail.com>
2021-06-15 20:02:21 -0300

    test: ensure SurveyList present correct username

--- src/main/test/cypress/integration/SurveyList.spec.ts
+++ src/main/test/cypress/integration/SurveyList.spec.ts
@@ -13,9 +13,16 @@ describe('SurveyList', () => {
     cy.getByTestId('error').should('container.text', 'Something wrong happened. Try again soon.')
   })
 
-  it('should present error on UnexpectedError', () => {
+  it('should logout on AccessDeniededError', () => {
     Http.mockAccessDeniedError()
     cy.visit('')
     Helper.testUrl('/login')
+  })
+
+  it('should present correct username', () => {
+    Http.mockUnexpectedError()
+    cy.visit('')
+    const { name } = Helper.getLocalStorageItem('@4Devs:account')
+    cy.getByTestId('error').should('container.text', name)
   })
 })
