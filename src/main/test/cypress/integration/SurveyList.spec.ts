ff5c9c07136743ed7c121bb1f1d7411def8c777e
Leandro2585 <leo.real2585@gmail.com>
2021-06-16 21:13:10 -0300

    test: ensure SurveyList relaod on button click

--- src/main/test/cypress/integration/SurveyList.spec.ts
+++ src/main/test/cypress/integration/SurveyList.spec.ts
@@ -1,8 +1,10 @@
 import * as Helper from '../utils/Helpers'
 import * as Http from '../utils/HttpMocks'
 
-const mockUnexpectedError = (): void => Http.mockServerError(/surveys/, 'POST')
-const mockAccessDeniedError = (): void => Http.mockForbiddenError(/surveys/, 'GET')
+const path = /surveys/
+const mockUnexpectedError = (): void => Http.mockServerError(path, 'POST')
+const mockAccessDeniedError = (): void => Http.mockForbiddenError(path, 'GET')
+const mockOk = (): void => Http.mockOk(path, 'GET', 'fx:survey-list')
 
 describe('SurveyList', () => {
   beforeEach(() => {
@@ -14,7 +16,16 @@ describe('SurveyList', () => {
   it('should present error on UnexpectedError', () => {
     mockUnexpectedError()
     cy.visit('')
-    cy.getByTestId('error').should('container.text', 'Something wrong happened. Try again soon.')
+    cy.getByTestId('error').should('contain.text', 'Something wrong happened. Try again soon.')
+  })
+
+  it('should reload on button click', () => {
+    mockUnexpectedError()
+    cy.visit('')
+    cy.getByTestId('error').should('contain.text', 'Something wrong happened. Try again soon.')
+    mockOk()
+    cy.getByTestId('reload').click()
+    cy.get('li:not(:empty)').should('have.length', 2)
   })
 
   it('should logout on AccessDeniededError', () => {
@@ -35,5 +46,24 @@ describe('SurveyList', () => {
     cy.visit('')
     cy.getByTestId('logout').click()
     Helper.testUrl('/login')
+  })
+
+  it('should present survey items', () => {
+    mockOk()
+    cy.visit('')
+    cy.get('li:empty').should('have.length', 4)
+    cy.get('li:not(:empty)').should('have.length', 2)
+    cy.get('li:nth-child(1)').then((li) => {
+      assert.equal(li.find('[data-testid="day"').text(), '03')
+      assert.equal(li.find('[data-testid="month"').text(), 'fev')
+      assert.equal(li.find('[data-testid="year"').text(), '2021')
+      assert.equal(li.find('[data-testid="question"').text(), 'Question 1')
+    })
+    cy.get('li:nth-child(2)').then((li) => {
+      assert.equal(li.find('[data-testid="day"').text(), '20')
+      assert.equal(li.find('[data-testid="month"').text(), 'out')
+      assert.equal(li.find('[data-testid="year"').text(), '2021')
+      assert.equal(li.find('[data-testid="question"').text(), 'Question 2')
+    })
   })
 })
