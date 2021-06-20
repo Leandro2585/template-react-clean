d896e74c6add418f7df006cd26357b42d9144638
Leandro2585 <leo.real2585@gmail.com>
2021-06-20 10:14:05 -0300

    feat: add SurveyResult layout

---
+++ src/shared/pages/SurveyResult.tsx
@@ -0,0 +1,31 @@
+import React from 'react'
+import FlipMove from 'react-flip-move'
+import { Header, Spinner } from '@shared/components'
+import Styles from '@shared/styles/surveyresult.scss'
+
+const SurveyResult: React.FC = () => {
+  return (
+    <div className={Styles.surveyResultWrap}>
+      <Header/>
+      <div className={Styles.contentWrap}>
+        <h2>Qual é seu framework web favorito</h2>
+        <FlipMove className={Styles.answersList}>
+          <li>
+            <img src="http://fordevs.herokuapp.com/static/img/logo-react.png"/>
+            <span className={Styles.answer}>ReactJS</span>
+            <span className={Styles.percent}>50%</span>
+          </li>
+        </FlipMove>
+        <button>Voltar</button>
+      </div>
+      <div className={Styles.loadingWrap}>
+        <div className={Styles.loading}>
+          <span>Aguarde...</span>
+          <Spinner isNegative/>
+        </div>
+      </div>
+    </div>
+  )
+}
+
+export default SurveyResult
--- src/shared/pages/index.tsx
+++ src/shared/pages/index.tsx
@@ -1,3 +1,4 @@
 export { default as Login } from './Login'
 export { default as SignUp } from './SignUp'
 export { default as SurveyList } from './SurveyList'
+export { default as SurveyResult } from './SurveyResult'
