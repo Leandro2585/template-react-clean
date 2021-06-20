53f143afa46acf57397005dfb701303ab1d71da7
Leandro2585 <leo.real2585@gmail.com>
2021-06-20 18:39:54 -0300

    refactor: move sut creation to a factory method

---
+++ src/domain/test/MockSurveyResult.ts
@@ -0,0 +1,23 @@
+import faker from 'faker'
+import { LoadSurveyResult } from '@domain/usecases'
+
+export const mockSurveyResultModel = (): LoadSurveyResult.Model => ({
+  question: faker.random.words(10),
+  date: faker.date.recent(),
+  answers: [{
+    image: faker.internet.url(),
+    answer: faker.random.word(),
+    count: faker.datatype.number(),
+    percent: faker.datatype.number(100),
+    isCurrentAccountAnswer: faker.datatype.boolean()
+  }]
+})
+
+export class LoadSurveyResultSpy implements LoadSurveyResult {
+  callsCount = 0
+  surveyResult: any
+  async load (): Promise<LoadSurveyResult.Model> {
+    this.callsCount++
+    return this.surveyResult
+  }
+}
--- src/shared/pages/SurveyResult.spec.tsx
+++ src/shared/pages/SurveyResult.spec.tsx
@@ -4,16 +4,24 @@ import { SurveyResult } from '@shared/pages'
 import { ApiContext } from '@shared/contexts'
 import { mockAccountModel } from '@domain/test'
 
+const makeSut = (): void => {
+  render(
+    <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
+        <SurveyResult/>
+    </ApiContext.Provider>
+  )
+}
+
 describe('SurveyResult Component', () => {
   test('should present correct initial state', () => {
-    render(
-      <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
-          <SurveyResult/>
-      </ApiContext.Provider>
-    )
+    makeSut()
     const surveyResult = screen.getByTestId('survey-result')
     expect(surveyResult.childElementCount).toBe(0)
     expect(screen.getByTestId('error')).not.toBeInTheDocument()
     expect(screen.getByTestId('loading')).not.toBeInTheDocument()
+  })
+
+  expect('should call present LoadSurveyResult', () => {
+
   })
 })
