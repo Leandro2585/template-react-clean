7a7fa10faa4450e741103f850653c9dfe456765c
Leandro2585 <leo.real2585@gmail.com>
2021-06-29 19:59:17 -0300

    test: ensure SurveyResult logout on AccessDeniedError

--- src/shared/pages/SurveyResult.spec.tsx
+++ src/shared/pages/SurveyResult.spec.tsx
@@ -1,23 +1,34 @@
 import React from 'react'
+import { Router } from 'react-router-dom'
+import { MemoryHistory, createMemoryHistory } from 'history'
 import { render, screen, waitFor } from '@testing-library/react'
 import { SurveyResult } from '@shared/pages'
 import { ApiContext } from '@shared/contexts'
 import { LoadSurveyResultSpy, mockAccountModel, mockSurveyResultModel } from '@domain/test'
-import { UnexpectedError } from '@domain/errors'
+import { AccessDeniedError, UnexpectedError } from '@domain/errors'
+import { AccountModel } from '@domain/models'
 
 type SutTypes = {
   loadSurveyResultSpy: LoadSurveyResultSpy;
+  history: MemoryHistory;
+  setCurrentAccountMock(account: AccountModel): void;
 }
 
 const makeSut = (loadSurveyResultSpy =  new LoadSurveyResultSpy()): SutTypes => {
+  const history = createMemoryHistory()
+  const setCurrentAccountMock = jest.fn()
   render(
-    <ApiContext.Provider value={{ setCurrentAccount: jest.fn(), getCurrentAccount: () => mockAccountModel() }}>
+    <ApiContext.Provider value={{ setCurrentAccount: setCurrentAccountMock, getCurrentAccount: () => mockAccountModel() }}>
+      <Router history={history}>
         <SurveyResult loadSurveyResult={loadSurveyResultSpy}/>
+      </Router>
     </ApiContext.Provider>
   )
 
   return {
-    loadSurveyResultSpy
+    loadSurveyResultSpy,
+    setCurrentAccountMock,
+    history
   }
 }
 
@@ -70,6 +81,14 @@ describe('SurveyResult Component', () => {
     expect(screen.queryByTestId('question')).not.toBeInTheDocument()
     expect(screen.getByTestId('error')).toHaveTextContent(error.message)
     expect(screen.queryByTestId('loading')).not.toBeInTheDocument()
+  })
 
+  test('should logout on AccessDeniedError', async () => {
+    const loadSurveyResultSpy = new LoadSurveyResultSpy()
+    jest.spyOn(loadSurveyResultSpy, 'load').mockRejectedValueOnce(new AccessDeniedError())
+    const { setCurrentAccountMock, history } = makeSut(loadSurveyResultSpy)
+    await waitFor(() => screen.getByTestId('survey-result'))
+    expect(setCurrentAccountMock).toHaveBeenCalledWith(undefined)
+    expect(history.location.pathname).toBe('/login')
   })
 })
