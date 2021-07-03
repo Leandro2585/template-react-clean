6e412e896742a745aeefdf9a5f9df2af1672c76a
Leandro2585 <leo.real2585@gmail.com>
2021-07-03 16:35:05 -0300

    test: ensure RemoteSaveSurveyResult returns UnexpectedError on 500

--- src/data/usecases/savesurveyresult/RemoteSaveSurveyResult.spec.ts
+++ src/data/usecases/savesurveyresult/RemoteSaveSurveyResult.spec.ts
@@ -46,4 +46,13 @@ describe('RemoteSaveSurveyResult', () => {
     const promise = sut.save(mockSaveSurveyResultParams())
     await expect(promise).rejects.toThrow(new UnexpectedError())
   })
+
+  test('should throw UnexpectedError if HttpClient returns 500', async () => {
+    const { sut, httpClientSpy } = makeSut()
+    httpClientSpy.response = {
+      statusCode: HttpStatusCode.serverError
+    }
+    const promise = sut.save(mockSaveSurveyResultParams())
+    await expect(promise).rejects.toThrow(new UnexpectedError())
+  })
 })
